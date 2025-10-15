92b71a7e746c936ac28e7c9b40a7b8f26bf5d2c4
fell <fellywi@gmail.com>
2025-10-15 21:08:48 +0700

    tessc.lua

--- tes sc.lua
+++ tes sc.lua
@@ -46,7 +46,7 @@ local Slider = PlayerTab:CreateSlider({
    Suffix = "Speed",
    CurrentValue = 10,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
    game.Player.LocalPlayer.Character:SetAttribute("SpeedMultiplier", Value)
    end,
+
 })
