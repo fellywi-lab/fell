0c3f3e04f32bd4c1691bc6e8871550c810ce2b64
fell <fellywi@gmail.com>
2025-10-16 20:01:42 +0700

    Update tes sc.lua

--- tes sc.lua
+++ tes sc.lua
@@ -6,30 +6,6 @@ local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
 local player = Players.LocalPlayer
 
-ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-
-   Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "https://discord.gg/Am3HvspbV6", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
-
 -------------------------------------------------
 -- ⚙️ Window Setup
 -------------------------------------------------
@@ -574,6 +550,7 @@ print("[Fish It Hub] AutoFishing + Webhook aktif.")
 
 -- 🔄 Load Dropdown
 RefreshDropdown()
+
 
 
 
